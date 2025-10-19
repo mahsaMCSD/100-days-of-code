d424f1596e0f0d5d441946341310241335ef21c1
mahsa <mahsaashrafi71@gmail.com>
2025-10-19 23:05:12 +0200

    day1

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("welcom to Band Name Generator")
+city=input("what is the name of city u grown up? \n")
+pet=input("what is the name of pet?\n")
+print("your band name is:"+" " + city + " " + pet)
